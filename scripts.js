eabfb897f06e50b188930fb8d92bad2fcbed2e35
Diego Mugnae <diegomugnae@gmail.com>
2021-10-21 19:09:10 +1000

    Add column to game

--- scripts.js
+++ scripts.js
@@ -9,4 +9,8 @@ const levels = ['easy','medium','hard']
 function addGenre(){
     const column = document.createElement('div')
     column.classList.add('genre-column')
+
+    column.innerHTML = ' this is the genre'
+
+    game.append(column)
 }
