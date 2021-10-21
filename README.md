6bce1958631f2a6a2ee93c8ce69e84da43bf3629
Diego Mugnae <diegomugnae@gmail.com>
2021-10-21 18:55:56 +1000

    style css

--- README.md
+++ README.md
@@ -6,3 +6,5 @@ https://www.youtube.com/watch?v=zgHim4ZDpZY
 
 Used API
 https://opentdb.com/api_config.php
+
+https://opentdb.com/api.php?amount=10&category=11&difficulty=easy&type=boolean
---
+++ scripts.js
@@ -0,0 +1,12 @@
+const game = document.getElementById('game')
+const scoreDisplay = document.getElementById('score')
+console.log(game,score)
+
+
+const film = 11
+const levels = ['easy','medium','hard']
+
+function addGenre(){
+    const column = document.createElement('div')
+    column.classList.add('genre-column')
+}
